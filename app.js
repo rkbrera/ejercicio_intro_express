6ac8d1757eb33be9d42c5bde887d293cc14c4679
Rodrigo Cabrera <rkbrk2928@gmail.com>
2023-08-16 22:08:24 -0600

    separating code into modules

--- app.js
+++ app.js
@@ -1,51 +1,14 @@
-const express = require("express");
+import express from "express"
+import userV1  from "./router/v1/users"
+import userV2 from "./router/v2/users"
+
+
 const app = express(); // Inicializamos clase que tiene todos los métodos y propiedades.
 const port = 3000;
 
-const usersArray = [
-  {
-    id: 1,
-    name: "Rafael",
-    lastName: "Quintero",
-    occupation: "Backend Mentor",
-  },
-  {
-    id: 2,
-    name: "Rodrigo",
-    lastName: "Cabrera",
-    occupation: "Koder",
-  },
+app.use("/v1", userV1)
+app.use("/v2", userV2)
 
-  {
-    id: 3,
-    name: "Fer",
-    lastName: "Kodemia",
-    occupation: "Coordi Gen26 JS",
-  },
-];
-
-// req = request = extraer info cliente (cuando me hablan)
-// res = response = enviar de vuelta info al cliente (cuando respondo)
-
-app.get("/v2/users", (req, res) => {
-  res.send(usersArray);
-});
-
-// New Endpoint
-app.get("/v2/users/:id", (req, res) => {
-  //  console.log("endpoint 2");
-  const userId = req.params.id;
-  console.log(`El id seleccionado es: ${userId}`);
-
-  if (userId) {
-    const getUserById = (id) => usersArray.find((user) => user.id === id);
-    const foundUser = getUserById(2);
-    console.log(foundUser);
-    res.status(200).send(foundUser);
-  } else {
-    res.status(404).send({ error: "No se encontro usuario" });
-  }
-});
 
 // inicializar puerto para escuchar peticiones
 app.listen(port, () => {
